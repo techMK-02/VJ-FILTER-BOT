ca3d737debc72574a5f3d51f772b4a3eeb333d62
Mohit <mohittatu386@gmail.com>
2025-06-14 09:35:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+A_-yDwUbIM9jODhl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/FilmyWabXGroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/FilmyXupdateS')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'filmyxsupport') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/icrunchbot')
