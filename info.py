7ad6f319aa4d2b0662282895122742b020ef11a4
Mohit <mohittatu386@gmail.com>
2025-06-16 10:41:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,8 +95,8 @@ OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/icrunchbot')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', False))
-BUTTON_MODE = bool(environ.get('BUTTON_MODE', False))
+PM_SEARCH = bool(environ.get('PM_SEARCH', True))
+BUTTON_MODE = bool(environ.get('BUTTON_MODE', True))
 MAX_BTN = bool(environ.get('MAX_BTN', True))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 IMDB = bool(environ.get('IMDB', False))
